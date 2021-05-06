e32f416675b94b0bcfa4a72aad9952a639fc515c
Vladislav Kozhevatov <envydef@gmail.com>
2021-05-07 02:17:26 +0300

    fix: validation

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getMovies, createMovie, deleteMovieById,
@@ -6,7 +7,21 @@ const {
 
 router.get('/', getMovies);
 
-router.post('/', createMovie);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().uri().required(),
+    trailer: Joi.string().uri().required(),
+    thumbnail: Joi.string().uri().required(),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}), createMovie);
 
 router.delete('/:movieId', deleteMovieById);
 
